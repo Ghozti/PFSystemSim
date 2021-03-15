9eef6edc47aa6a24a4e3b1bb0b7a119dbdc2a288
Ghozti <ybalderas1005@gmail.com>
2021-03-15 10:11:49 -0400

    filled in CardMethods

--- src/pfsystem/structure/cards/Card.java
+++ src/pfsystem/structure/cards/Card.java
@@ -1,4 +1,24 @@
 package pfsystem.structure.cards;
 
-public class Card {
+public class Card implements CardMethods{
+
+    @Override
+    public void setExpirationDate(String date) {
+
+    }
+
+    @Override
+    public void setID(String ID) {
+
+    }
+
+    @Override
+    public String getID() {
+        return null;
+    }
+
+    @Override
+    public String getExpirationDate() {
+        return null;
+    }
 }
--- src/pfsystem/structure/cards/CardMethods.java
+++ src/pfsystem/structure/cards/CardMethods.java
@@ -1,4 +1,12 @@
 package pfsystem.structure.cards;
 
 public interface CardMethods {
+
+    void setExpirationDate(String date);
+
+    void setID(String ID);
+
+    String getID ();
+
+    String getExpirationDate();
 }
