268ad02b285b66c36570d4f682fa7b1318f407d9
Ghozti <ybalderas1005@gmail.com>
2021-03-16 12:18:46 -0400

    filled in Guest

---
+++ src/pfsystem/structure/user/Guest.java
@@ -0,0 +1,12 @@
+package pfsystem.structure.user;
+
+public class Guest {
+
+    int age;
+    String name;
+
+    public Guest(int age, String name){
+        this.age = age;
+        this.name = name;
+    }
+}
---
+++ src/pfsystem/structure/user/User.java
@@ -0,0 +1,19 @@
+package pfsystem.structure.user;
+
+import pfsystem.structure.cards.BlackCard;
+import pfsystem.structure.cards.WhiteCard;
+
+public class User {
+
+    public User(WhiteCard whiteCard, int age, String name){
+
+    }
+
+    public User(BlackCard blackCard, int age, String name){
+
+    }
+
+    public User(BlackCard blackCard, int age, String name, Guest guest){
+
+    }
+}
