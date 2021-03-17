036e7324dbe3160beed8dd957bd8a666ec5e04a4
Ghozti <ybalderas1005@gmail.com>
2021-03-16 21:08:30 -0400

    worked on UI and fxml button methods in MainController

--- src/pfsystem/fxmlcontrollers/MainController.java
+++ src/pfsystem/fxmlcontrollers/MainController.java
@@ -1,4 +1,19 @@
 package pfsystem.fxmlcontrollers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class MainController {
+
+    @FXML
+    public void accountView(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void moreView(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void deactivateView(ActionEvent actionEvent) {
+    }
 }
