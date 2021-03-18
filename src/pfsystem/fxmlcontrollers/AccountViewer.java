a89f34fdfe67b532a509c2ad77ad8d209449c57e
Ghozti <ybalderas1005@gmail.com>
2021-03-18 16:15:28 -0400

    filled in AccountViewer.java

--- src/pfsystem/fxmlcontrollers/AccountViewer.java
+++ src/pfsystem/fxmlcontrollers/AccountViewer.java
@@ -1,4 +1,24 @@
 package pfsystem.fxmlcontrollers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class AccountViewer {
+
+    @FXML
+    Label myCard;
+
+    @FXML
+    Label myName;
+
+    @FXML
+    Label cardID;
+
+    @FXML
+    Label massageAvailable;
+
+    @FXML
+    public void checkOut(ActionEvent actionEvent) {
+    }
 }
