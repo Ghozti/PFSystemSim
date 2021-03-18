7ecb2b3c57288402a8872190206ad03c6b90bf7a
Ghozti <ybalderas1005@gmail.com>
2021-03-17 21:33:39 -0400

    filled in DataBaseCreator

--- src/pfsystem/utils/DataBaseCreator.java
+++ src/pfsystem/utils/DataBaseCreator.java
@@ -1,16 +1,25 @@
 package pfsystem.utils;
 
+import java.io.File;
+
 public class DataBaseCreator {
 
     public void createMainDir(){
-
+        File directory = new File(System.getProperty("user.home") + "/PFSystem");
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
     }
 
-    public void createSubDir(){
-
+    public String createSubDir(String name){
+        File directory = new File(System.getProperty("user.home") + "/PFSystem/" + name);
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+        return directory.getAbsolutePath();
     }
 
-    public void createFile(){
+    public void createFile(String path){
 
     }
 }
