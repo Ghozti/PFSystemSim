e351e043922b53cb73f5d89f2cf03f332a94a8af
Ghozti <ybalderas1005@gmail.com>
2021-03-17 21:13:27 -0400

    added the changeScene method in Main

--- src/pfsystem/Main.java
+++ src/pfsystem/Main.java
@@ -6,15 +6,28 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("fxml/main.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Home Page");
         primaryStage.centerOnScreen();
+        primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root, 1200, 800));
         primaryStage.show();
+    }
+
+    public void changeScene(String sceneName, String titleName, boolean resize, int w, int h) throws IOException {
+        Stage stg = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/" + sceneName + ".fxml"));
+        stg.setTitle(titleName);
+        stg.centerOnScreen();
+        stg.setResizable(resize);
+        stg.setScene(new Scene(root,w,h));
+        stg.show();
     }
 
     public static void main(String[] args) {
--- src/pfsystem/fxmlcontrollers/MainController.java
+++ src/pfsystem/fxmlcontrollers/MainController.java
@@ -2,11 +2,17 @@ package pfsystem.fxmlcontrollers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import pfsystem.Main;
+
+import java.io.IOException;
 
 public class MainController {
 
+    private Main main = new Main();
+
     @FXML
-    public void accountView(ActionEvent actionEvent) {
+    public void accountView(ActionEvent actionEvent) throws IOException {
+        main.changeScene("CheckIn","Account viewer",false,600,400);
     }
 
     @FXML
