c59609bec8cd9616f9633fcdccfe59218ef98b88
Ghozti <ybalderas1005@gmail.com>
2021-03-20 12:57:30 -0400

    added the messages class in constants

--- src/pfsystem/fxmlcontrollers/CreateCard.java
+++ src/pfsystem/fxmlcontrollers/CreateCard.java
@@ -1,4 +1,22 @@
 package pfsystem.fxmlcontrollers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class CreateCard {
+
+    @FXML
+    TextField nameField;
+
+    @FXML
+    TextField dateField;
+
+    @FXML
+    Label label;
+
+    @FXML
+    public void createCard(ActionEvent actionEvent) {
+    }
 }
---
+++ src/pfsystem/utils/constants/Messages.java
@@ -0,0 +1,4 @@
+package pfsystem.utils.constants;
+
+public class Messages {
+}
