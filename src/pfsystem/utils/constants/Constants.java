64003c7e3a3ccd2137ffdecf94a1f9bf7c8a9670
Ghozti <ybalderas1005@gmail.com>
2021-03-24 21:04:23 -0400

    -added more constants

--- src/pfsystem/utils/constants/Constants.java
+++ src/pfsystem/utils/constants/Constants.java
@@ -4,13 +4,24 @@ public class Constants {
 
     /**PATHS**/
     public static final String p_mainPath = System.getProperty("user.home") + "/" + "PFsystem";
+
     public static final String p_userPath = System.getProperty("user.home") + "/" + "PFsystem/User";
+
     public static final String p_cardPath = System.getProperty("user.home") + "/" + "PFsystem/User/Card";
+
+    public static final String p_cardDatePath = System.getProperty("user.home") + "/" + "PFsystem/User/Card/date.txt";
+
+    public static final String p_cardNamePath = System.getProperty("user.home") + "/" + "PFsystem/User/Card/name.txt";
+
     public static final String p_cardIDPath = System.getProperty("user.home") + "/" + "PFsystem/User/Card/ID.txt";
+
     public static final String p_typePath = System.getProperty("user.home") + "/" + "PFsystem/User/Card/type.txt";
+
     public static final String p_activatePath = System.getProperty("user.home") + "/" + "PFsystem/User/Card/active.txt";
+
     /**ERRORS**/
     public static final String e_invalidInput = "Invalid Input";
+
     public static final String e_EmptyFields = "These fields cannot be empty!";
 
     /**MESSAGES**/
