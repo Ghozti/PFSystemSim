8b4cf9ccbd3bca27605dd5dd723c16909cca45c8
Ghozti <ybalderas1005@gmail.com>
2021-03-28 11:04:04 -0400

    - fixed label issue in DeactivateScreen.fxml

--- src/pfsystem/fxmlcontrollers/DeactivateController.java
+++ src/pfsystem/fxmlcontrollers/DeactivateController.java
@@ -4,20 +4,18 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import pfsystem.utils.constants.Constants;
 import pfsystem.utils.database.DataBaseCreator;
-
-import java.awt.*;
 import java.io.File;
 
 public class DeactivateController {
 
     @FXML
-    Label confirmLabel;
+    public javafx.scene.control.Label mLabel;
 
     public void deactivate(ActionEvent event) {
         DataBaseCreator.deleteFile(new File(Constants.Paths.p_cardIDPath));
         DataBaseCreator.deleteFile(new File(Constants.Paths.p_typePath));
         DataBaseCreator.deleteFile(new File(Constants.Paths.p_cardNamePath));
         DataBaseCreator.deleteFile(new File(Constants.Paths.p_cardDatePath));
-        confirmLabel.setText(Constants.Messages.m_confirmDeactivate);
+        mLabel.setText(Constants.Messages.m_confirmDeactivate);
     }
 }
